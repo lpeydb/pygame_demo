2411ec9f19a325d2e5f1534f1382938b2854a055
lpeydb <2535155641@qq.com>
2022-10-19 21:45:44 +0800

    抠图删掉背景

--- fly_bird/main.py
+++ fly_bird/main.py
@@ -6,13 +6,13 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 FPS = 60
 BIRD_WIDTH, BIRD_HEIGHT = 55, 40
-PILLAR_WIDTH, PILLAR_HEIGHT = 50,200
+PILLAR_WIDTH, PILLAR_HEIGHT = 50, 200
 FALL_VEL = 2
 FLY_VEL = 10
-PILLAR_VEL = 5
+PILLAR_VEL = 30
 
 PILLAR_MOVE = pygame.USEREVENT + 1
-PILLAR_MOVE_TIME = 1000
+PILLAR_MOVE_TIME = 500
 pygame.time.set_timer(PILLAR_MOVE, PILLAR_MOVE_TIME)
 
 BACKGROUND = pygame.transform.scale(
@@ -22,9 +22,11 @@ BIRD = pygame.transform.scale(
 PILLAR = pygame.transform.scale(
     pygame.image.load(os.path.join('fly_bird/figures', 'pillar.png')), (PILLAR_WIDTH, PILLAR_HEIGHT))
 
+
 def bird_handle_movement(keys_pressed, bird):
-    if keys_pressed[pygame.K_SPACE] :  # FLY
+    if keys_pressed[pygame.K_SPACE]:  # FLY
         bird.y -= FLY_VEL
+
 
 def draw_window(bird, pillar):
     WIN.blit(BACKGROUND, (0, 0))
@@ -33,10 +35,6 @@ def draw_window(bird, pillar):
     WIN.blit(PILLAR, (pillar.x, pillar.y))
 
     pygame.display.update()
-
-
-
-
 
 
 def main():
@@ -51,17 +49,16 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            
+
             if event.type == PILLAR_MOVE:
                 pillar.x -= PILLAR_VEL
 
         keys_pressed = pygame.key.get_pressed()
         bird_handle_movement(keys_pressed, bird)
-        
+
         bird.y += FALL_VEL
 
         draw_window(bird, pillar)
-
 
 
 if __name__ == "__main__":
